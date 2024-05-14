9b34f5d30f6be6beb1e8e86dd1b94a19d0366fe7
trivasor <13akstjq.blog23@gmail.com>
2024-05-14 17:09:43 +0900

    init

---
+++ scripts/deploy.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env sh
+
+ls
+
+# 정적 sitemap 생성
+echo "정적 sitemap 생성중.."
+node scripts/sitemap.js
+echo "정적 sitemap 생성 완료!"
+
+# abort on errors
+set -e
+
+# build
+yarn build
+
+# navigate into the build output directory
+cd out
+
+# gh-pages clone
+git clone -b gh-pages https://github.com/trivasor/trivasor.github.io/
+cp -rf trivasor.github.io/.git ./.git
+git config user.name "trivasor"
+git config user.email "13akstjq.blog24@gmail.com"
+rm -rf trivasor.github.io
+
+touch .nojekyll
+
+# if you are deploying to a custom domain
+# echo 'www.example.com' > CNAME
+
+git add .
+git commit -m "add post"
+
+# if you are deploying to https://<USERNAME>.github.io
+# git push -f git@github.com:ui-log/ui-log.github.io.git master
+
+# if you are deploying to https://<USERNAME>.github.io/<REPO>
+git push origin gh-pages
+
+cd -
